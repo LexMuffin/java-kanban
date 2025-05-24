8e932d9e8f5959ef4bbb223b38c7f85cad720335
Mihail <kto-kuda-a-ti-suda@yandex.ru>
2025-05-24 13:44:09 +0300

    Добавлено решение для ФЗ-8. Исправление 1.

--- src/exceptions/ManagerTimeConflictException.java
+++ src/exceptions/ManagerTimeConflictException.java
@@ -1,6 +1,6 @@
 package exceptions;
 
-public class ManagerTimeConflictException extends RuntimeException{
+public class ManagerTimeConflictException extends RuntimeException {
     public ManagerTimeConflictException(String message) {
         super(message);
     }
